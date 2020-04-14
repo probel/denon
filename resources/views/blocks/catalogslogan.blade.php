0c4c2f357acd69990af11b51ca014bde0f55c82f
JZUSER <call89269081096@gmail.com>
2020-04-14 09:56:20 +0300

    make top slogal bar (3)

--- resources/views/blocks/catalogslogan.blade.php
+++ resources/views/blocks/catalogslogan.blade.php
@@ -1,8 +1,8 @@
 <section class="slogan">
     <div class="container">
-        <h2 class="slogan__title text-uppercase text-center">ТОЧНОСТЬ - КЛЮЧЕВОЙ АСПЕКТ ПРИ ВОСПРОИЗВЕДЕНИИ МУЗЫКИ</h2>
-        <div class="slogan__text text-uppercase text-center mx-auto"> Уникальные технологии Denon для воспроизведения
-            CD/SACD дисков, строгий отбор комплектующих и мастерство схемотехники помогают воспроизводить звук,
-            максимально приближенный к оригиналу.</div>
+        <h2 class="slogan__title text-uppercase text-center">{{  $values["slogan"]  ?? '' }}</h2>
+        <div class="slogan__text text-uppercase text-center mx-auto">
+            {!! $values["description"] ?? '' !!}
+        </div>
     </div>
 </section>
